860e32bbaf09dae36230dde438ca542a2b8ab5cc
St.Sergey.Minsky <Sterlikovsergey@icloud.com>
2024-02-04 23:47:44 +0300

    start project

---
+++ src/main/java/by/st/monitoringandanalyzingexchangerates/MonitoringAndAnalyzingExchangeRatesApplication.java
@@ -0,0 +1,13 @@
+package by.st.monitoringandanalyzingexchangerates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringAndAnalyzingExchangeRatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringAndAnalyzingExchangeRatesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/st/monitoringandanalyzingexchangerates/MonitoringAndAnalyzingExchangeRatesApplicationTests.java
@@ -0,0 +1,13 @@
+package by.st.monitoringandanalyzingexchangerates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringAndAnalyzingExchangeRatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
